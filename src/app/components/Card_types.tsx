57a47532a052c175da0f3b6456b71588cdbe0c44
juanc <juancamilo1478@gmail.com>
2025-05-23 23:22:17 -0500

    Save

--- src/app/components/Card_types.tsx
+++ src/app/components/Card_types.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { AppData } from "../datas";
 
 import 'swiper/css';
- 
+
 type CardServiceProps = {
   data: AppData
 };
@@ -12,11 +12,11 @@ type CardServiceProps = {
 export function Card_movile_1({ data }: CardServiceProps) {
   const { descripcion, name, image_one, } = data;
   return (
-    <div className="bg-gray-300 shadow-md rounded-lg flex-grow overflow-hidden mb-5 md:mb-0 hover:border-blue-500 hover:border-2  cursor-pointer">
-      <div className="flex justify-between items-center w-full mx-auto">
+    <div className="bg-gray-300  shadow-md rounded-lg flex-grow overflow-hidden mb-5 md:mb-0 hover:border-blue-500 hover:border-2  cursor-pointer">
+      <div className="flex justify-between items-center w-full mx-auto relative h-full">
         {/* Contenido de texto */}
-        <div className="w-[40%] ml-10 ">
-          <h3 className="text-xl font-bold text-zinc-800   font-nunito-extra-bold">{name}</h3>
+        <div className="w-[45%] ml-10 sm:py-20 z-10">
+          <h3 className="text-xl font-bold text-zinc-800   font-nunito-extra-bold mb-5">{name}</h3>
           <p className="text-gray-700  ">{descripcion}</p>
         </div>
         {/* Imagen a la derecha */}
@@ -25,8 +25,8 @@ export function Card_movile_1({ data }: CardServiceProps) {
           src={image_one}
           alt={name}
           width={500}
-          height={300}
-          className="w-full h-full object-cover rounded-lg"
+          height={200}
+          className="w-[50%] h-full object-cover rounded-lg absolute right-0 top-0 z-0"
         />
 
       </div>
@@ -35,9 +35,9 @@ export function Card_movile_1({ data }: CardServiceProps) {
 }
 
 export function Card_desktop({ data }: CardServiceProps) {
-  const { descripcion, name,   link,     image_one } = data;
+  const { descripcion, name, link, image_one } = data;
   return (
-    <div className="w-full min-h-[300px] cursor-pointer  bg-white   hover:border-blue-500 hover:border-2 p-6 rounded-2xl shadow-[0px_4px_12px_rgba(0,0,0,0.1)] hover:shadow-[0px_8px_16px_rgba(0,0,0,0.2)] transition duration-300 flex items-center mb-5 md:mb-0 ">
+    <div className="w-full min-h-[300px] cursor-pointer  bg-white    hover:border-blue-500 hover:border-2 p-6 rounded-2xl shadow-[0px_4px_12px_rgba(0,0,0,0.1)] hover:shadow-[0px_8px_16px_rgba(0,0,0,0.2)] transition duration-300 flex items-center mb-5 md:mb-0 ">
       {/* Contenido de texto */}
       <div className="w-[50%] p-4">
         <h3 className="text-xl font-bold text-zinc-800   font-nunito-extra-bold">{name}</h3>
@@ -68,9 +68,9 @@ export function Card_desktop({ data }: CardServiceProps) {
 }
 
 export function Card_desktop_2({ data }: CardServiceProps) {
-  const { descripcion, name,   link,     image_one } = data;
+  const { descripcion, name, link, image_one } = data;
   return (
-    <div className="w-full min-h-[300px]  hover:border-blue-500 hover:border-2  cursor-pointer bg-blue-300 p-6 rounded-2xl shadow-[0px_4px_12px_rgba(0,0,0,0.1)] hover:shadow-[0px_8px_16px_rgba(0,0,0,0.2)] transition duration-300 flex items-center">
+    <div className="w-full min-h-[300px]  hover:border-blue-500 hover:border-2  cursor-pointer bg-blue-300   p-6 rounded-2xl shadow-[0px_4px_12px_rgba(0,0,0,0.1)] hover:shadow-[0px_8px_16px_rgba(0,0,0,0.2)] transition duration-300 flex items-center">
 
       <div className="w-[50%] p-4">
         <h3 className="text-xl font-bold text-zinc-800 font-nunito-extra-bold">{name}</h3>
